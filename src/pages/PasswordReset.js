84c06d0b651e818e79036846e9b0ebdede9b1f46
Raouf <abgh@odoo.com>
2022-04-30 16:57:00 +0100

    Add Password reset ui

--- src/pages/PasswordReset.js
+++ src/pages/PasswordReset.js
@@ -1,101 +1,88 @@
-import { StyledFormButton, StyledFormArea, 
-    StyledTextInput, StyledLabel, 
-    StyledTitle, Avatar, colors,
-    ButtonGroup,
-    StyledButton,
-    ExtraText,
-    TextLink,
-    CopyrightText
-} from "../components/Styles";
-import Logo from "./../assets/logo.png";
 import * as Yup from 'yup'
 
-import {Formik, Form} from 'formik'
-import { TextInput } from "../components/FormLib";
-import { FiMail, FiLock, FiUser } from 'react-icons/fi';
-import { ThreeDots } from 'react-loader-spinner';
-
+import {Formik, Field, ErrorMessage} from 'formik'
 import { connect } from "react-redux";
 import { resetPassword } from "../auth/actions/userAction";
 import { useSearchParams, useNavigate } from "react-router-dom"
 
-const PasswordReset = ({ resetPassword }) => {
+const PasswordReset = ({forgottenPassword}) => {
     const navigate = useNavigate()
     const [searchParams, setSearchParams] = useSearchParams();
     const reset_password_code = searchParams.get("reset_password_code");
     return (
-        <div>
-            <StyledFormArea>
-                <Avatar image={Logo} widthSize={200} heightSize={50}/>
-                <StyledTitle color={colors.dark1} size={30}>
-                    Reset Password
-                </StyledTitle>
-                <Formik
-                    initialValues={{
-                        new_password: "",
-                        confirm_new_password: "",
-                        reset_password_code
-                    }}
-                    validationSchema={
-                        Yup.object({
-                            new_password: Yup.string()
-                                .min(3, "Password is too short")
-                                .max(30, "password is too long")
-                                .required("Required field"),
-                            confirm_new_password: Yup.string()
-                                .required("Required field")
-                                .oneOf([Yup.ref("new_password")], "Passwords must match")
-                        })
-                    }
-                    onSubmit={(values, {setSubmitting, setFieldError}) => {
-                        console.log(values)
-                        resetPassword(values, navigate, setFieldError, setSubmitting);
-                    }}
-                >
-                    {( {isSubmitting} ) => (
-                        <Form>
-                            <TextInput
-                                name="new_password"
-                                type="password"
-                                label="Password"
-                                placeholder="***********"
-                                icon={<FiLock/>}
-                            />
-                            <TextInput
-                                name="confirm_new_password"
-                                type="password"
-                                label="Confirm Password"
-                                placeholder="***********"
-                                icon={<FiLock/>}
-                            />
-                            <ButtonGroup>
-                                {!isSubmitting && (
-                                        <StyledFormButton type="submit">
-                                            Submit
-                                        </StyledFormButton>
-                                    ) 
-                                 }
-
-                                {isSubmitting && (
-                                        <ThreeDots
-                                            color={colors.dark2}
-                                            height={49}
-                                            width={100}
-                                        />
-                                    )
+		<div class="login-area">
+			<div class="login-item2">
+				<a href="/altimate-platform-web/home"><img src="images/logo.png" alt="" /></a>
+			</div>
+			<Formik
+                initialValues={{
+                    new_password: "",
+                    confirm_new_password: "",
+                    reset_password_code
+                }}
+                validationSchema={
+                    Yup.object({
+                        new_password: Yup.string()
+                            .min(3, "Password is too short")
+                            .max(30, "password is too long")
+                            .required("Required field"),
+                        confirm_new_password: Yup.string()
+                            .required("Required field")
+                            .oneOf([Yup.ref("new_password")], "Passwords must match")
+                    })
+                }
+                onSubmit={(values, {setSubmitting, setFieldError}) => {
+                    console.log(values)
+                    resetPassword(values, navigate, setFieldError, setSubmitting);
+                }}
+            >
+                        {({ handleSubmit,
+                            isSubmitting, 
+                        }) => (
+                        <div class="login-item">
+				            <div class="login-item-inner">
+                                <form onSubmit={handleSubmit}>
+                                    <div class="login-item-inner2">
+                                        <h3>Welcome to <a href="/altimate-platform-web/home">Altimate AI</a></h3>
+                                        <div>
+                                            <p>No Account ?<br></br><a href="/altimate-platform-web/signup">Sign up</a></p>
+                                        </div>
+                                    </div>
+                                    <label>Enter your Password</label>
+                                <div class="login-item-inner4">
+                                    <Field name="new_password" type="password" id="txtPassword"/>
+                                        <button type="button" onClick={() => {
+                                                    var type = document.getElementById('txtPassword').type;
+                                                    document.getElementById('txtPassword').type = type === 'password' ? 'text' : 'password';
+                                                }
+                                            }
+                                               class="toggle">
+                                        <i id="eyeIcon" class="fa fa-eye"></i>
+                                        </button>
+                                </div>
+                                <ErrorMessage name="password" component="div" />
+                                <label>Confirm  Password</label>
+                                <div class="login-item-inner4">
+                                <Field name="confirm_new_password" type="password" id="txtConfirmPassword"/>
+                                <button type="button" onClick={() => {
+                                    var type = document.getElementById('txtConfirmPassword').type;
+                                    document.getElementById('txtConfirmPassword').type = type === 'password' ? 'text' : 'password';
+                                    }
                                 }
-                            </ButtonGroup>
-                        </Form>
-                    )}
-                </Formik>
-                <ExtraText>
-                    Already have an account ? <TextLink to="/login">Login</TextLink>
-                </ExtraText>
-            </StyledFormArea>
-            <CopyrightText>
-                All rights reserved &copy;2022 by Altimate.ai
-            </CopyrightText>
-        </div>
+                                    class="toggle">
+                                    <i id="eyeIcon" class="fa fa-eye"></i>
+                                    </button>
+                                </div>
+                                <ErrorMessage name="confirm_password" component="div" />
+                                <button type="submit" disabled={isSubmitting}>
+                                    Submit
+                                </button>
+                                </form>
+                            </div>
+                        </div>
+                )}
+            </Formik>
+	</div>
     )
 }
 
